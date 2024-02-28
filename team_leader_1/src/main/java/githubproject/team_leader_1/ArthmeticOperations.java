9279de4a9f034383d01c562bc9660190c8f222f6
Akshay <akshaybhutkar51@gmail.com>
2024-02-28 08:30:56 +0530

    changes

--- team_leader_1/src/main/java/githubproject/team_leader_1/ArthmeticOperations.java
+++ team_leader_1/src/main/java/githubproject/team_leader_1/ArthmeticOperations.java
@@ -3,15 +3,9 @@ package githubproject.team_leader_1;
 public class ArthmeticOperations {
 	public static void main(String args[]) {
 		//Write some arthematic operations methods
+		Operations op=new Operations();
+		op.add(99, 33);
 	}
-	public static float add(float one,float two) {
-		return one+two;
-	}
-	public static float subtraction(float one,float two) {
-		return one-two;
-	}
-	public static float division(float one,float two) {
-		return one/two;
-	}
+
 
 }
---
+++ team_leader_1/src/main/java/githubproject/team_leader_1/Operations.java
@@ -0,0 +1,12 @@
+package githubproject.team_leader_1;
+
+public class Operations {
+	int a;
+	int b;
+	public int add(int a,int b) {
+		this.a=a;
+		this.b=b;
+		return a+b;
+	}
+
+}
